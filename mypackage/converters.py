4a6f198cd96751adfc820692bf29044755817ff7
RhodyArthur <rhodaarthur48@gmail.com>
2026-01-07 16:37:38 +0000

    feat: add km_to_miles conversion function

--- mypackage/converters.py
+++ mypackage/converters.py
@@ -1,9 +1,18 @@
+# `converters.py` should contain: `celsius_to_fahrenheit(c)`, `km_to_miles(km)`, `kg_to_pounds(kg)`
+
 def celsius_to_fahrenheit(c):
     """
     Covert temperature from celsius to fahrenheit
-    
     :param c: temperature in celsius
     """
     return (c * 9/5) + 32
 
+def km_to_miles(km):
+    """
+    convert disrance from kilometers to miles
+    :param km: distance in km
+    """
+    return km * 0.6214
+
 print(celsius_to_fahrenheit(25))
+print(km_to_miles(1))
