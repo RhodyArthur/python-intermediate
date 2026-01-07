7d939f67c88f20af3e007851ffea488107cc304e
RhodyArthur <rhodaarthur48@gmail.com>
2026-01-07 16:33:50 +0000

    feat: add celsius to fahrenheit conversion function

---
+++ mypackage/converters.py
@@ -0,0 +1,9 @@
+def celsius_to_fahrenheit(c):
+    """
+    Covert temperature from celsius to fahrenheit
+    
+    :param c: temperature in celsius
+    """
+    return (c * 9/5) + 32
+
+print(celsius_to_fahrenheit(25))
